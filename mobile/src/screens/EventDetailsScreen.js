94f509bd869d8f7852f979edb1a9e412ed5f76c7
Ganisetti Lakshmi Visalakshi <24A95A0517@aec.edu.in>
2026-09-04 14:39:19 +0530

    Update EventDetailsScreen.js

--- mobile/src/screens/EventDetailsScreen.js
+++ mobile/src/screens/EventDetailsScreen.js
@@ -369,7 +369,11 @@ export default function EventDetailsScreen({ route, navigation }) {
                   {r.paymentStatus === 'pending' && (
                     <TouchableOpacity 
                       style={{ backgroundColor: COLORS.primary, padding: 8, borderRadius: 5, marginTop: 5, alignItems: 'center' }}
-                      onPress={() => handleApprovePayment(r.user?._id || r.user)}
+                      onPress={() => {
+                      console.log('REGISTRATION OBJECT:', JSON.stringify(r));
+                      console.log('REGISTRATION USER:', JSON.stringify(r.user));
+                      handleApprovePayment(r.user?._id || r.user);
+                    }}
                     >
                       <Text style={{ color: COLORS.white, fontWeight: 'bold' }}>Approve Payment</Text>
                     </TouchableOpacity>
